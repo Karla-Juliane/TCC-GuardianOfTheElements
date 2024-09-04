e42a8a14495e5d48a44178990eef5e7ba6b224e1
maria vitoria <mendes.vitoria@escolar.ifrn.edu.br>
2024-09-04 11:39:31 -0300

    Espinho

--- Guardian Of The Elements/Assets/Elements/Scripts/Player/PlayerController.cs
+++ Guardian Of The Elements/Assets/Elements/Scripts/Player/PlayerController.cs
@@ -85,6 +85,12 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
+    public void Demage(int damage)
+    {
+        life -= damage;
+    }
+    
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground");

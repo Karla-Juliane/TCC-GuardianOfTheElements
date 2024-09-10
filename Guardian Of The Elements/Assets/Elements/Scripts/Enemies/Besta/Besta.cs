abce8749462abccc886e1ce4b412338847e04b83
Maria Vitoria Santos Mendes <mendes.vitoria@academico.ifrn.edu.br>
2024-09-10 11:50:28 -0300

    inimigo

--- Guardian Of The Elements/Assets/Elements/Scripts/Enemies/Besta/Besta.cs
+++ Guardian Of The Elements/Assets/Elements/Scripts/Enemies/Besta/Besta.cs
@@ -6,39 +6,68 @@ using UnityEngine;
 
 public class Besta : MonoBehaviour
 {
-    [SerializeField] private Transform alvo;
-    [SerializeField] private float velocidadeMovimento;
-    [SerializeField] private float distanciaMinima;
-    [SerializeField] private Rigidbody2D rigidbody;
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Transform alvo; // Alvo a seguir
+    [SerializeField] private float velocidadeMovimento; // Velocidade de movimento do inimigo
+    [SerializeField] private float distanciaMinima; // Distância mínima para parar de se mover
+    [SerializeField] private Rigidbody2D rigidbody; // Rigidbody para aplicar movimento
+    [SerializeField] private SpriteRenderer spriteRenderer; // SpriteRenderer para inverter sprite
+    [SerializeField] private float distanciaVisao; // Distância máxima de visão
+    [SerializeField] private float anguloVisao; // Ângulo de visão em graus
 
-    // Update is called once per frame
+    // Update is called uma vez por frame
     void Update()
     {
-        Vector2 posicaoAlvo = this.alvo.position;
-        Vector2 posicaoAtual = this.transform.position;
-
-        float distancia = Vector2.Distance(posicaoAtual, posicaoAlvo);
-
-        if (distancia >= this.distanciaMinima)
+        if (AlvoEstaNoCampoDeVisao())
         {
-            Vector2 direcao = posicaoAlvo - posicaoAtual;
-            direcao.Normalize();
+            Vector2 posicaoAlvo = this.alvo.position;
+            Vector2 posicaoAtual = this.transform.position;
 
-            this.rigidbody.velocity = this.velocidadeMovimento * direcao;
+            float distancia = Vector2.Distance(posicaoAtual, posicaoAlvo);
 
-            if (this.rigidbody.velocity.x > 0)
+            if (distancia >= this.distanciaMinima)
             {
-                this.spriteRenderer.flipX = false;
+                Vector2 direcao = posicaoAlvo - posicaoAtual;
+                direcao = direcao.normalized;
+
+                this.rigidbody.velocity = (this.velocidadeMovimento * direcao);
+
+                // Inverte o sprite baseado na direção
+                if (this.rigidbody.velocity.x > 0)
+                {
+                    this.spriteRenderer.flipX = false;
+                }
+                else if (this.rigidbody.velocity.x < 0)
+                {
+                    this.spriteRenderer.flipX = true;
+                }
             }
-            else if (this.rigidbody.velocity.x < 0)
+            else
             {
-                this.spriteRenderer.flipX = true;
+                this.rigidbody.velocity = Vector2.zero; // Para o movimento quando estiver próximo o suficiente
             }
         }
         else
         {
-            this.rigidbody.velocity = Vector2.zero;
+            this.rigidbody.velocity = Vector2.zero; // Se o alvo não estiver no campo de visão, não se move
         }
+    }
+
+    // Função para verificar se o alvo está dentro do campo de visão
+    bool AlvoEstaNoCampoDeVisao()
+    {
+        Vector2 direcaoAlvo = this.alvo.position - this.transform.position;
+        float distanciaAlvo = direcaoAlvo.magnitude;
+
+        // Verifica se o alvo está dentro da distância de visão
+        if (distanciaAlvo > this.distanciaVisao)
+        {
+            return false;
+        }
+
+        // Verifica se o alvo está dentro do ângulo de visão
+        direcaoAlvo.Normalize();
+        float anguloEntreInimigoEAlvo = Vector2.Angle(this.transform.right, direcaoAlvo);
+
+        return anguloEntreInimigoEAlvo < this.anguloVisao / 2f;
     }
 }
